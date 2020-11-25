1952b272708fa3dd4ba48ee5d0d4387760920f4d
madCat00 <francescoxx35@gmail.com>
2020-11-25 15:28:03 +0100

    This is the first program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci il tuo nome");
+            string name = Console.ReadLine();
+            string toUpper = name.ToUpper();
+            System.Console.WriteLine( "Il nome inserito in maiuscolo è" +" "+ toUpper);
         }
     }
 }
