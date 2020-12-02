f4057617d22246bf750d5d3b97f4f6ee03c61349
madCat00 <francescoxx35@gmail.com>
2020-12-02 10:11:25 +0100

    Create metod CheckAge and verify the age of a user. Comunicate with other class Article and verify if users can buy any articles ;closes #3

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ namespace CSharpExercises
         Console.WriteLine("The invoice is {0}",article1.Invoice);//Get the value of invoice "0"
 
         Customer Antonio = new Customer ("Antoni","Rossi","@gmail.com");
-        var age = Antonio.Age = 25;
+        var age = Antonio.Age = 22;
         Antonio.CheckAge();
         Article wine = new Article(23,23.44,age);
         wine.Create();
